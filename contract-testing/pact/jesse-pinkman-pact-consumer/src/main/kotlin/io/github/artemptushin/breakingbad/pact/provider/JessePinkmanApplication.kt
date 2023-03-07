b3de22d9abcb54537050971d1d22eb9e5e8f9f26
Artem Ptushkin <artem_ptushkin@iptiq.com>
2023-03-07 15:27:50 +0100

    contract tests

---
+++ contract-testing/pact/jesse-pinkman-pact-consumer/src/main/kotlin/io/github/artemptushin/breakingbad/pact/provider/JessePinkmanApplication.kt
@@ -0,0 +1,11 @@
+package io.github.artemptushin.breakingbad.pact.provider
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JessePinkmanApplication
+
+fun main(args: Array<String>) {
+    runApplication<JessePinkmanApplication>(*args)
+}
