8cbe6f68bc0c08036bc5b842a4b65b4e7bd78143
Artem Ptushkin <artem_ptushkin@iptiq.com>
2023-03-09 13:54:34 +0100

    consumer - broker - provider works

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Breaking the API bad
+
+## Contract testing with Pact
+
+### Consumer
+
+```shell
+./gradlew :contract-testing:pact:jesse-pinkman-pact-consumer:contractTest
+
+./gradlew :contract-testing:pact:jesse-pinkman-pact-consumer:pactPublish -Ppactbroker.auth.token=$SAUL_GOODMAN_BROKER_TOKEN -Ppact.publish.tags=main -Ppact.publish.consumerVersion=jesse-season1
+```
+
+### Provider
+
+```shell
+./gradlew contractTest :contract-testing:pact:heisenberg-pact-service:contractTest \
+  -Ppactbroker.auth.token=$SAUL_GOODMAN_BROKER_TOKEN \
+  -Ppactbroker.consumerversionselectors.tags=main \
+  -Ppact.verifier.publishResults=true \
+  -Ppact.provider.tag=main \
+  -Ppact.provider.version=heisenberg-season1 -i -s
+```
---
+++ contract-testing/pact/jesse-pinkman-pact-consumer/src/contractTest/kotlin/io/github/artemptushin/breakingbad/pact/provider/service/HeisenbergServiceTest.kt
@@ -0,0 +1,66 @@
+package io.github.artemptushin.breakingbad.pact.provider.service
+
+import au.com.dius.pact.consumer.dsl.LambdaDsl
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt
+import au.com.dius.pact.consumer.junit5.PactTestFor
+import au.com.dius.pact.core.model.V4Pact
+import au.com.dius.pact.core.model.annotations.Pact
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.web.client.RestTemplate
+
+@PactTestFor(port = "8091", providerName = "heisenberg")
+@ExtendWith(value = [SpringExtension::class, PactConsumerTestExt::class])
+class HeisenbergServiceTest {
+
+    lateinit var heisenbergService: HeisenbergService
+
+    @BeforeEach
+    fun setup() {
+        heisenbergService = HeisenbergService(RestTemplate())
+    }
+
+    @Pact(consumer = "jesse-pinkman")
+    fun crystalsPact(builder: PactDslWithProvider): V4Pact {
+        return builder
+            .given("this guy cooks!")
+            .uponReceiving("Crystals, beach!")
+            .method("GET")
+            .path("/heisenberg/v1/crystals")
+            .matchQuery("amount", ".d*", "2")
+            .willRespondWith()
+            .body(
+                LambdaDsl.newJsonBody { bodyDsl ->
+                    bodyDsl
+                        .numberType("amount", 20.0)
+                        .array("crystals") { crystals ->
+                            crystals
+                                .`object` { crystal ->
+                                    crystal
+                                        .numberType("id", 1)
+                                        .stringType("color", "red")
+                                }
+                                .`object` { crystal ->
+                                    crystal
+                                        .numberType("id", 2)
+                                        .stringType("color", "blue")
+                                }
+                        }
+                }.build()
+            )
+            .status(200)
+            .toPact(V4Pact::class.java)
+    }
+
+    @Test
+    fun `this-guy-cooks!`() {
+        val crystals = heisenbergService.cookCrystals(2)
+
+        assertThat(crystals).isNotNull
+        assertThat(crystals.crystals).hasSize(2)
+    }
+}
